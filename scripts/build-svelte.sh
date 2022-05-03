49be01cc49204d422e5f8e23789cb47dd04e50f8
Daison Carino <daison12006013@gmail.com>
2022-05-03 20:46:52 +0800

    Initialize

---
+++ scripts/build-svelte.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Firing up svelte-kit!"
+(
+    rm -rf .svelte-kit/ \
+    && npm install \
+    && LUCID_DOCKER_SETTING_UP=1 ROUTE_FOLDER=guest npm run build \
+    && LUCID_DOCKER_SETTING_UP=1 ROUTE_FOLDER=docs npm run build \
+)
+echo "Finished!"
